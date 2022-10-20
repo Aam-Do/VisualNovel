b4045e1c7d7ba53325f6e6468179077f6bdb33c5
Am.D <ameliedelloro@gmail.com>
2022-10-20 18:51:32 +0200

    changes from lesson

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -3,6 +3,48 @@ namespace Template {
   export import ƒS = FudgeStory;
   console.log("FudgeStory template starting");
 
+  // Preparation for needed media -> put into definitions
+
+  export let transition = {
+    puzzle: {
+      duration: 1,
+      alpha: "path",
+      edge: 1
+    }
+  };
+
+  export let sound = {
+    // themes
+    music: "path",
+    // SFX
+    drop: "path"
+  };
+
+  export let locations = {
+    beachDay: {
+      name: "Beach Day",
+      background: "path"
+    }
+  };
+
+  export let characters = {
+    narrator: {
+      name: "path"
+    },
+    protagonist: {
+      name: "path"
+    },
+    aisaka: {
+      name: "Aisaka",
+      origin: ƒS.ORIGIN.BOTTOMCENTER,
+      pose: {
+        angry: "path",
+        happy: "path",
+        upset: "path"
+      }
+    }
+  }
+
   export let dataForSave = {
     nameProtagonist: ""
   };
