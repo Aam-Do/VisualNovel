25002da597cebea3c7529b444b85fcc699148ab2
Am.D <ameliedelloro@gmail.com>
2022-10-20 18:21:52 +0200

    changes on main from lesson

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -1,14 +1,20 @@
 namespace Template {
   export import ƒ = FudgeCore;
   export import ƒS = FudgeStory;
-
   console.log("FudgeStory template starting");
+
+  export let dataForSave = {
+    nameProtagonist: ""
+  };
 
   window.addEventListener("load", start);
   function start(_event: Event): void {
     let scenes: ƒS.Scenes = [
       { scene: Scene, name: "Scene" }
     ];
+
+    let uiElement: HTMLElement = document.querySelector("[type=interface]");
+    dataForSave = ƒS.Progress.setData(dataForSave, uiElement);
 
     // start the sequence
     ƒS.Progress.go(scenes);
